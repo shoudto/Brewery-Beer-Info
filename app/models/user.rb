0529366e214de440f1325f05f50dcdbf43414f2f
Oscar Shoudt <shoudto@gmail.com>
2020-01-12 08:19:30 -0500

    creating a logout function

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     has_many :reviews
     has_many :reviewed_beers, through: :reviews, source: :beer # renaming a relationships
+    validates :username, uniqueness: true, presence: true
+    has_secure_password
 end

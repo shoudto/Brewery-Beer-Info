b8c326b1df3f91c0180bd3076db14fc343abd7ed
Oscar Shoudt <shoudto@gmail.com>
2020-01-12 07:35:47 -0500

    Setting up the Belongs_to and Has_many relationships

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,6 @@
 class Beer < ApplicationRecord
   belongs_to :brewery
-  belongs_to :user
-end
+  belongs_to :user # creator of it
+  has_many :reviews
+  has_many :users, through :reviews # people who have reviewed it
+end 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    has_many :reviews
+    has_many :reviewed_beers, through :reviews, source: :beer # renaming a relationships
 end

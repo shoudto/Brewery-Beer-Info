84fb505794021a47eba27be009ccf3a17be5ee9c
Oscar Shoudt <shoudto@gmail.com>
2020-01-12 09:15:00 -0500

    creating form to add a beer

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,5 +1,22 @@
 class BeersController < ApplicationController
     def new 
         @beer = Beer.new
+        @beer.build_brewery
+    end 
+
+    def create
+        @beer = Beer.new(beer_params)
+
+        if @beer.save  
+            redirect_to ice_cream_path(@beer)
+        else 
+            render :new 
+        end 
+    end 
+
+    private 
+
+    def beer_params
+        params.require(:beer).permit(:name, :style, :alc, :description, brewery_attributes: [:name])
     end 
 end
--- app/models/beer.rb
+++ app/models/beer.rb
@@ -2,5 +2,6 @@ class Beer < ApplicationRecord
   belongs_to :brewery
   belongs_to :user # creator of it
   has_many :reviews
-  has_many :users, through :reviews # people who have reviewed it
+  has_many :users, through: :reviews # people who have reviewed it
+  accepts_nested_attributes_for :brewery
 end 
